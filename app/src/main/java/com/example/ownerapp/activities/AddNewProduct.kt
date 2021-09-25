40af3c8f884314425b3b12d62dcf8f1a8f02aab8
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-09-25 14:25:05 +0530

    add product layout done

--- app/src/main/java/com/example/ownerapp/activities/AddNewProduct.kt
+++ app/src/main/java/com/example/ownerapp/activities/AddNewProduct.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -11,7 +12,6 @@ import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.GetMultipleContents
 import androidx.appcompat.app.AlertDialog
@@ -19,8 +19,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
+import com.example.ownerapp.Adapters.ImageSliderAdapter
 import com.example.ownerapp.R
 import com.example.ownerapp.data.Product
+import com.example.ownerapp.data.SliderItem
 import com.example.ownerapp.databinding.ActivityAddNewProductBinding
 import com.example.ownerapp.di.components.DaggerFactoryComponent
 import com.example.ownerapp.di.modules.FactoryModule
@@ -28,6 +30,10 @@ import com.example.ownerapp.di.modules.RepositoryModule
 import com.example.ownerapp.mvvm.repository.MainRepository
 import com.example.ownerapp.mvvm.viewmodles.MainViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController.ClickListener
+import com.smarteist.autoimageslider.SliderAnimations
+import com.smarteist.autoimageslider.SliderView
 
 
 class AddNewProduct : AppCompatActivity() {
@@ -37,7 +43,7 @@ class AddNewProduct : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var component: DaggerFactoryComponent
     var position = 0
-
+    private var adapter: ImageSliderAdapter? = null
 
     var getContent = registerForActivityResult(GetMultipleContents()) { it ->
         binding.ImageLay.visibility = View.VISIBLE
@@ -50,9 +56,8 @@ class AddNewProduct : AppCompatActivity() {
             }
 
         }
+        renewItems(binding.sliderView)
         Log.d(TAG, "Selected Images are ${arrayListImages.size}: ")
-        binding.imageSwitcherAdd.setImageURI(arrayListImages[0])
-        binding.imageSwitcherAdd.visibility = View.VISIBLE
         position = 0
     }
 
@@ -62,11 +67,20 @@ class AddNewProduct : AppCompatActivity() {
         setContentView(binding.root)
 
         init()
+        initSlider()
 
-        binding.imageSwitcherAdd.setFactory {
-            val imageView = ImageView(applicationContext)
-            imageView
-        }
+        binding.sliderView.setOnIndicatorClickListener(ClickListener {
+            Log.i(
+                "GGG",
+                "onIndicatorClicked: " + binding.sliderView.currentPagePosition
+            )
+        })
+
+//        binding.imageSwitcherAdd.setFactory {
+//            val imageView = ImageView(applicationContext)
+//            imageView
+//        }
+
         binding.pickImages.setOnClickListener {
             if (ActivityCompat.checkSelfPermission(
                     this,
@@ -99,45 +113,25 @@ class AddNewProduct : AppCompatActivity() {
                                     startActivity(it)
                                     finish()
                                 }
-                            } else {
+                            } else
                                 Toast.makeText(this, "Select Product Images", Toast.LENGTH_SHORT)
                                     .show()
-                            }
-                        } else {
+
+                        } else
                             Toast.makeText(this, "Enter Description", Toast.LENGTH_SHORT).show()
-                        }
-                    } else {
+
+                    } else
                         Toast.makeText(this, "Select The Category", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
+
+                } else
                     Toast.makeText(this, "Enter Product Price", Toast.LENGTH_SHORT).show()
-                }
-            } else {
+
+            } else
                 Toast.makeText(this, "Enter Product name", Toast.LENGTH_SHORT).show()
-            }
 
         }
 
 
-        binding.backButton.setOnClickListener {
-            Log.d(TAG, "Back $position size = ${arrayListImages.size}")
-            if (position > 0) {
-                position--
-                binding.imageSwitcherAdd.setImageURI(arrayListImages[position])
-            }
-        }
-
-        binding.frontButton.setOnClickListener {
-            Log.d(TAG, "Front $position")
-            if (position < arrayListImages.size) {
-                binding.imageSwitcherAdd.setImageURI(arrayListImages[position])
-                position++
-            }
-        }
-
-        binding.submitProduct.setOnClickListener {
-
-        }
     }
 
     private fun checkForPermissions() {
@@ -229,4 +223,42 @@ class AddNewProduct : AppCompatActivity() {
     }
 
 
+    private fun initSlider() {
+        adapter = ImageSliderAdapter()
+        adapter!!.setContext(this)
+        binding.sliderView.setSliderAdapter(adapter!!)
+        binding.sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);//set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        binding.sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
+        binding.sliderView.autoCycleDirection = SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH
+        binding.sliderView.indicatorSelectedColor = Color.WHITE
+        binding.sliderView.indicatorUnselectedColor = Color.GRAY
+        binding.sliderView.scrollTimeInSec = 3
+        binding.sliderView.isAutoCycle = true
+        binding.sliderView.startAutoCycle()
+    }
+
+
+    fun renewItems(view: View?) {
+        val sliderItemList: MutableList<SliderItem> = ArrayList()
+        //dummy data
+        for (i in 0 until arrayListImages.size) {
+            val sliderItem = SliderItem()
+            sliderItem.description = "Slider Item $i"
+            sliderItem.imageUrl= arrayListImages[i].toString()
+            sliderItemList.add(sliderItem)
+        }
+        adapter!!.renewItems(sliderItemList as ArrayList<SliderItem>)
+    }
+
+    fun removeLastItem(view: View?) {
+        if (adapter!!.count - 1 >= 0) adapter!!.deleteItem(adapter!!.count - 1)
+    }
+
+    fun addNewItem(view: View?) {
+        val sliderItem = SliderItem()
+        sliderItem.description = "Slider Item Added Manually"
+        sliderItem.imageUrl =
+            "https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        adapter!!.addItem(sliderItem)
+    }
 }
