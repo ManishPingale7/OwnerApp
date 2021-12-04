3923cb765f2c957c0703702b926eb75dccc71098
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-12-04 16:12:19 +0530

    fonts removed

--- app/src/main/java/com/example/ownerapp/Fragments/mainFrags/Home.kt
+++ app/src/main/java/com/example/ownerapp/Fragments/mainFrags/Home.kt
@@ -1,12 +1,14 @@
 package com.example.ownerapp.Fragments.mainFrags
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.ownerapp.activities.ViewUsers
 import com.example.ownerapp.databinding.FragmentHomeBinding
 import com.example.ownerapp.di.components.DaggerFactoryComponent
 import com.example.ownerapp.di.modules.FactoryModule
@@ -32,6 +34,9 @@ class Home : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         init()
 
+        binding.usersGainedMonth.setOnClickListener {
+            startActivity(Intent(requireContext(),ViewUsers::class.java))
+        }
         return binding.root
     }
 
